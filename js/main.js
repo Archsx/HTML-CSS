036156de51d7c041b16cd40cb8b163c349234844
suxun <871728578@qq.com>
2018-11-15 01:43:30 +0800

    mytune

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(function() {
+	menu = $('nav ul');
+
+  $('#openup').on('click', function(e) {
+    e.preventDefault(); menu.slideToggle();
+  });
+  
+  $(window).resize(function(){
+    var w = $(this).width(); if(w > 768 && menu.is(':hidden')) {
+      menu.removeAttr('style');
+    }
+  });
+  
+  $('nav li').on('click', function(e) {                
+    var w = $(window).width(); if(w < 768 ) {
+      menu.slideToggle(); 
+    }
+  });
+  // $('.open-menu').height($(window).height());
+
+  $('.cf a').on('click',function(e){
+    if(this.hash !== ''){
+      e.preventDefault();
+      const hash = this.hash
+      $("html,body").animate({
+        scrollTop:$(hash).offset().top
+      },800,function(){
+        window.location.hash = hash
+      })
+    }
+  })
+
+
+
+
+});
